b7f3dd0c8d14de0d0febd05b14cb654478710d2c
Jason Lee <12leeejk2@gmail.com>
2023-04-09 03:32:03 +0100

    added PKCE authentication

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template, request, flash
-import time
+from flask import Flask, flash, make_response, render_template, request, session, redirect, url_for
 import util
 
 app = Flask(__name__)
-app.secret_key = 'ajweffas'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
+# immediate authentication
 def index():
-    return render_template("index.html")
+    request = util.get_auth_code()
+    app.config['CODE_VERIFIER'] = request[1]
+    print("STORED CODE VERIFIER: ", app.config.get('CODE_VERIFIER'))
+    response = make_response(redirect(request[0]))
+    return response
+
+@app.route('/callback')
+def callback():
+    auth_code = request.args.get('code')
+    code_verifier = app.config.get('CODE_VERIFIER')
+    print("RETRIEVED CODE VERIFIER: ", app.config.get('CODE_VERIFIER'))
+
+    access_token, refresh_token = util.get_token_pkce(auth_code, code_verifier)
+    session['access_token'] = access_token
+    session['refresh_token'] = refresh_token
+
+    return render_template('index.html')
 
 @app.route('/track_id', methods=['POST'])
 def predict():
+    access_token = session.get('access_token')
     track_id = request.form['track_id_input']
-    prediction = util.predict_vibe(track_id)
+    prediction = util.predict_vibe(access_token, track_id)
     flash("Result: " + prediction)
     return render_template("index.html", track_id=track_id, prediction=prediction)
 
--- util.py
+++ util.py
@@ -1,13 +1,102 @@
-from dotenv import load_dotenv
-import pandas as pd
+import hashlib, string, secrets
+from requests import get, post, Request
+import base64
+from api_util import get_audio_features
 import numpy as np
 import pickle
-import os
-import time
 
-from api_util import get_audio_features, get_token
+def generate_randomstring(n):
+    """
+    Generates random string
+    Arguments:
+        n: length of string
+    Returns:
+        A randomly generated string
+    """
+    alphabet = string.ascii_letters + string.digits
+    code_verifier = ''.join(secrets.choice(alphabet) for i in range(n))
+    return code_verifier
+
+def generate_code_challenge(code_verifier):
+    """
+    Transforms code verifier using SHA256 algorithm, creating digest
+    Then the digest is encoded into base64
+    Arguments:
+        code_verifier: the code verifier
+    Returns:
+        Base64 encoding of digest - serves as the code challenge
+    """
+    code_challenge = hashlib.sha256(code_verifier.encode('utf-8')).digest()
+    code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8')
+    code_challenge = code_challenge.replace('=', '')
+
+    return code_challenge
+
+def get_auth_code():
+    """
+    Creates an authorisation code to send for an access token.
+    Returns:
+        Authorisation code
+    """
+    url = "https://accounts.spotify.com/authorize?"
+    scope = "playlist-modify-public"
+    redirect_uri = "http://localhost:5000/callback"
+    state = generate_randomstring(16)
+    code_verifier = generate_randomstring(128)
+    code_challenge = generate_code_challenge(code_verifier)
+
+    params = {
+        "response_type": "code",
+        "client_id": "af2795e2f63545f3883b5ab47e29187a",
+        "scope": scope,
+        "redirect_uri": redirect_uri,
+        "state": state,
+        "code_challenge_method": "S256",
+        "code_challenge": code_challenge
+        }
+
+    request = Request('GET', url, params=params)
+    request = request.prepare()
+    
+    return request.url, code_verifier
+
+def get_token_pkce(auth_code, code_verifier):
+
+    url = "https://accounts.spotify.com/api/token"
+    redirect_uri = "http://localhost:5000/callback"
+
+    headers = {
+        "Content_Type" : "application/x-www-form-urlencoded"
+    }
+
+    data = {
+        "grant_type" : "authorization_code",
+        "code" : auth_code,
+        "client_id" : "af2795e2f63545f3883b5ab47e29187a",
+        "redirect_uri": redirect_uri,
+        "code_verifier": code_verifier}
+    
+    request = Request('POST', url, headers=headers, data=data)
+    request = request.prepare()
+
+    result = post(url, headers=headers, data=data)
+    print("Result: ", result.content)
+    json_result = result.json()
+    access_token = json_result["access_token"]
+    refresh_token = json_result["refresh_token"]
+
+    return access_token, refresh_token
 
 def prepare_prediction(token, track_id):
+    """
+    Gets audio features, given an access token and track ID
+    and returns and prepared Numpy array
+    Arguments:
+        token: access token
+        track_id: Track ID
+    Returns:
+        Prepared Numpy array of shape (1, 10) to pass into model
+    """
 
     audio_features = get_audio_features(token, track_id)
     X = np.zeros((1,10))
@@ -28,21 +117,13 @@ def prepare_prediction(token, track_id):
     
     except:
         raise Exception
-
-def predict_vibe(track_id):
-
-    # Load .env variables
-    load_dotenv()
-    client_id = os.getenv("CLIENT_ID")
-    client_secret = os.getenv("CLIENT_SECRET")
-
-    # get access token
-    token = get_token(client_id, client_secret)
+    
+def predict_vibe(access_token, track_id):
 
     # prepare data
     while True:
         try:
-            X = prepare_prediction(token, track_id)
+            X = prepare_prediction(access_token, track_id)
             break
         except:
             return "Something went wrong - please check your track ID and try again."
@@ -61,4 +142,3 @@ def predict_vibe(track_id):
         response = PREDICTION_LIST[2]
         
     return response
-
