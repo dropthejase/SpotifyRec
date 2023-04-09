1e439c28ac4e0ff394d137ab55c917847ad8444e
Jason Lee <12leeejk2@gmail.com>
2023-04-09 15:37:16 +0100

    login functionality

--- app.py
+++ app.py
@@ -5,33 +5,63 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
-# immediate authentication
 def index():
-    request = util.get_auth_code()
-    app.config['CODE_VERIFIER'] = request[1]
-    print("STORED CODE VERIFIER: ", app.config.get('CODE_VERIFIER'))
-    response = make_response(redirect(request[0]))
-    return response
+    return render_template("index.html")
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == 'POST':
+        # store user name in session
+        user = request.form['user']
+        session['user'] = user
+
+        # redirect to Spotify login
+        get_auth_code = util.get_auth_code()
+        app.config['CODE_VERIFIER'] = get_auth_code[1]
+        response = make_response(redirect(get_auth_code[0]))
+        return response
+    else:
+        return render_template("index.html")
 
 @app.route('/callback')
 def callback():
     auth_code = request.args.get('code')
     code_verifier = app.config.get('CODE_VERIFIER')
-    print("RETRIEVED CODE VERIFIER: ", app.config.get('CODE_VERIFIER'))
+
+    print("SESSION: ", session)
+    user = session.get('user')
 
     access_token, refresh_token = util.get_token_pkce(auth_code, code_verifier)
     session['access_token'] = access_token
     session['refresh_token'] = refresh_token
 
-    return render_template('index.html')
+    return render_template('track_id.html', user=user)
 
-@app.route('/track_id', methods=['POST'])
-def predict():
-    access_token = session.get('access_token')
-    track_id = request.form['track_id_input']
-    prediction = util.predict_vibe(access_token, track_id)
-    flash("Result: " + prediction)
-    return render_template("index.html", track_id=track_id, prediction=prediction)
+@app.route('/track_id', methods=['POST', 'GET'])
+def track_id():
+    if request.method == 'POST':
+        if 'user' in session:
+            user = session.get('user')
+            access_token = session.get('access_token')
+            track_id = request.form['track_id_input']
+
+            if track_id == "":
+                flash("Please enter a track ID into the box above")
+                return render_template("track_id.html", user=user)
+            
+            else:
+                prediction = util.predict_vibe(access_token, track_id)
+                flash("Result: " + prediction)
+                return render_template("track_id.html", user=user, track_id=track_id, prediction=prediction)
+        
+        else:
+            return redirect(url_for("login"))
+    else:
+        if 'user' in session:
+            user = session.get('user')
+            return render_template('track_id.html', user=user)
+        else:
+            return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
